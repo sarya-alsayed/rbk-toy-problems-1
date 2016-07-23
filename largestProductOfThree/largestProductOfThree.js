d5b1f238a55487105fc4f5735ad966caa6385259
sarya-alsayed <e.saryaa@gmail.com>
2016-07-23 09:05:07 +0300

    largest product

--- largestProductOfThree/largestProductOfThree.js
+++ largestProductOfThree/largestProductOfThree.js
@@ -6,6 +6,26 @@
  *
  * Extra credit: Make your function handle negative numbers.
  */
-
+var bubbleSort = function(array) {
+	for (var i=0;i<array.length;i++){
+		for (var j=0;j<array.length;j++){
+		   if (array[j]<array[j+1]){
+		   	var temp=array[j];
+		   	array[j]=array[j+1];
+		   	array[j+1]=temp;
+		   }
+		}
+	}
+   return array;
+};
 var largestProductOfThree = function(array) {
+var newArr=bubbleSort(array);
+console.log(newArr);
+var product=newArr[0]*newArr[1]*newArr[2];
+return product;
+  
+	
+
+
+
 };
