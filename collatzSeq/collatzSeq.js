afa51796a91a034e266c44ea2ee957dcca102d0f
sarya-alsayed <e.saryaa@gmail.com>
2016-07-28 09:49:46 +0300

    solve longestColla

--- collatzSeq/collatzSeq.js
+++ collatzSeq/collatzSeq.js
@@ -13,9 +13,40 @@ Which starting number, under one million, produces the longest chain?
 
 NOTE: Once the chain starts the terms are allowed to go above one million.
 */
+var CollatzSeq=function(startNumber){
+	var array=[];
+	array.push(startNumber);
+    while(startNumber!==1){
+    	if (startNumber%2===0){
+    		startNumber=startNumber/2;
+    		array.push(startNumber);
+    	}
+    	else if (startNumber%2!==0){
+    		startNumber=3*startNumber+1;
+    		array.push(startNumber);
+         }
+    }
+    //console.log(array) ;
+    return array.length;
+
+}
+
 
 var longestCollatzSeq = function(){
-  return 0;
+	var n=1000000;
+	var max=0;
+	for (var i=1;i<=n;i++){
+		var chainLength=CollatzSeq(i);
+        if (chainLength>max){
+        	var number=i;
+        	max=chainLength;
+
+        }
+
+	}
+	console.log(max);
+
+  return number;
 }
 
 
