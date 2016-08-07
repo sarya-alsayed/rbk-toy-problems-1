e2c92dac83ca7987f663c70fc4708a11965662ed
sarya-alsayed <e.saryaa@gmail.com>
2016-08-07 09:53:06 +0300

    array

--- majorityElement/majorityElement.js
+++ majorityElement/majorityElement.js
@@ -9,5 +9,23 @@ findAll([17,4,12,-16,12,4,7,12]) //should return [12]
 */
 
 var findAll = function(arr){
+	var c=Math.floor(arr.length/3);
+	var counter=0;
+	var result=[];
+    for (var i=0;i<arr.length;i++){
+	    counter=0;
+    	for (var j=0;j<arr.length;j++){
+
+    		if (arr[i]===arr[j]){
+    			counter++
+    		}
+    	}
+    		if (counter	>c){
+    			
+    			result.push(arr[i]);
+    		}
+    	
+    }
+   return result;	
 
 }
