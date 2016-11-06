e817b58d5424da23b28b77eecf3879e1b13eb173
sarya-alsayed <e.saryaa@gmail.com>
2016-11-06 11:22:51 +0200

    update solution

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -32,4 +32,43 @@ var rockPaperScissors = function() {
    return array;
 };
 
+var rockPaperScissors1 = function (){
+	 var arrayGame=["rock","paper","scissors"];
+   var array=[];
+  for (var i=0;i<arrayGame.length;i++){
+   	for (var j=0; j< arrayGame.length;j++){
+      for (var k=0;k<arrayGame.length;k++){
+        debugger;
+        array.push([].concat(arrayGame[i],arrayGame[j],arrayGame[k]));
+      }
+   	}
+  }
+   return array;
 
+}
+//// solution from pavel
+var rockPaperScissors2 = function (/* START SOLUTION */rounds/* END SOLUTION */) {
+  // TODO: your solution here
+  /* START SOLUTION */
+    rounds = rounds || 3;
+    var outcomes = [];
+
+    var plays = ['rock', 'paper', 'scissors'];
+
+    var combos = function(roundsToGo, playedSoFar) {
+      if( roundsToGo === 0 ){
+        outcomes.push( playedSoFar );
+        return;
+      }
+
+      for( var i = 0; i < plays.length; i++ ){
+        var currentPlay = plays[i];
+        combos( roundsToGo-1, playedSoFar.concat(currentPlay) );
+      }
+    };
+    combos( rounds, [] );
+
+    return outcomes;
+  /* END SOLUTION */
+};
+///http://www.collinwu.me/rock-paper-scissors-iterative-vs-recursive-implementation/
